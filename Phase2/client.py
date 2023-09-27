eacbb217f5a0025e2b493b9402e1b74dfa5117c7
Isra Chokwatana <113561482+Isra3750@users.noreply.github.com>
2023-09-27 14:44:57 -0400

    Update client.py

--- Phase2/client.py
+++ Phase2/client.py
@@ -1,12 +1,41 @@
-import socket       #Python socket library
-server_address = socket.gethostbyname(socket.gethostname())     #Gets the host name of the current system
-server_port = 12000         #Server port
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    #Creates UDP socket
-client_socket.connect((server_address, server_port))    #Connects to the server
+import socket
 
-message = input('Message to be echoed: ')     #The message to be sent to the server
-print("Client Message:",message)        #Prints out the message before sending it
-client_socket.sendto(message.encode(),(server_address,server_port))     #Sends the message to the server
-modifiedMessage,server_address=client_socket.recvfrom(12000)        #Recieves the modified message from the server
-print("Server Echo:",modifiedMessage.decode())      #Prints out the message echoed back from the server
-print(server_address)           #Prints the server address
+# Function to create packets from the image file
+def Make_Packet(file):
+    packet_size = 1024
+    packets = []
+    while True:
+        data = file.read(packet_size)
+        if not data:
+            break
+        packets.append(data)  # Append the data as a packet (1024b) into packets list
+    return packets
+
+# Create a UDP socket for client
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSocket.bind(('127.0.0.1', 12002))  # Bind the client socket to the client local address
+
+# Server address and port
+server_address = ('127.0.0.1', 12000)
+
+# Open a file for reading the image data
+file = open('sample.bmp', 'rb')
+
+# Send packets to the server, for each packet in list, send one by one
+i = 0
+print('Sending...')
+for packet in Make_Packet(file):
+    # Send data to the server
+    clientSocket.sendto(packet, server_address)
+
+    #print out current progress / byte
+    print(str(i * 1024) + ' bytes')
+    i = i + 1
+
+# Send an end-of-transmission flag
+clientSocket.sendto(b'END_OF_TRANSMISSION', server_address)
+print('Done!')
+
+# Close file and client socket
+file.close()
+clientSocket.close()
