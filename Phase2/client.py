a80628b101b2f395a64a281b222dbaa4f2989558
Isra Chokwatana <113561482+Isra3750@users.noreply.github.com>
2023-09-30 18:26:23 -0400

    Update client.py

--- Phase2/client.py
+++ Phase2/client.py
@@ -1,18 +1,18 @@
-import socket
+from socket import *
 
 # Function to create packets from the image file
 def Make_Packet(file):
-    packet_size = 1024
+    packet_size = 1024 # 1024 byte chunks
     packets = []
     while True:
-        data = file.read(packet_size)
+        data = file.read(packet_size) # Seperate file into chunks base on packet size
         if not data:
             break
         packets.append(data)  # Append the data as a packet (1024b) into packets list
     return packets
 
 # Create a UDP socket for client
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.bind(('127.0.0.1', 12002))  # Bind the client socket to the client local address
 
 # Server address and port
@@ -22,18 +22,14 @@ server_address = ('127.0.0.1', 12000)
 file = open('sample.bmp', 'rb')
 
 # Send packets to the server, for each packet in list, send one by one
-i = 0
 print('Sending...')
 for packet in Make_Packet(file):
     # Send data to the server
     clientSocket.sendto(packet, server_address)
 
-    #print out current progress / byte
-    print(str(i * 1024) + ' bytes')
-    i = i + 1
 
-# Send an end-of-transmission flag
-clientSocket.sendto(b'END_OF_TRANSMISSION', server_address)
+# Send an end-of-transmission flag in byte
+clientSocket.sendto(b'END_OF_FILE', server_address)
 print('Done!')
 
 # Close file and client socket
