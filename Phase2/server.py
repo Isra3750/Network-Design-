94c81aa3270c7efe1835039fdd1d65f1f83c7543
Isra Chokwatana <113561482+Isra3750@users.noreply.github.com>
2023-09-30 18:26:35 -0400

    Update server.py

--- Phase2/server.py
+++ Phase2/server.py
@@ -1,9 +1,9 @@
-import socket
+from socket import *
 
-# Create a UDP socket
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Create a UDP socket for server
+serverSocket = socket(AF_INET, SOCK_DGRAM)
 
-# Bind the socket to the server address and port
+# Bind the socket to the server address and port num
 serverSocket.bind(('127.0.0.1', 12000)) # use 192.168.1.22 (local ipv4) if there's 2 computer
 
 # Open a file for writing the received image data
@@ -12,16 +12,16 @@ file = open('output.bmp', 'wb')
 print('Checking...')
 while True:
     # Receive data from the client, one packet at a time until END message
-    image_chunk, client_address = serverSocket.recvfrom(1024)
+    data, client_address = serverSocket.recvfrom(1024)
     
-    # When done, break out loop
-    if image_chunk == b'END_OF_TRANSMISSION':
+    # If done, break out loop
+    if data == b'END_OF_FILE':
         break
 
-    #Write to file
-    file.write(image_chunk)
+    # Write to file called output.bmp
+    file.write(data)
 
-print("Received from cilent port number " + str(client_address[1])) #confirm recieve
+print("Received from cilent port number " + str(client_address[1])) # Confirm recieved
 
 # Close file and server socket
 file.close() 
