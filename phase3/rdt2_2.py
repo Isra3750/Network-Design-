caf21c3c11683e34e77fc564bbc1786aa8a621fd
Isra Chokwatana <113561482+Isra3750@users.noreply.github.com>
2023-10-25 11:10:20 -0400

    Add files via upload

--- phase3/rdt2_2.py
+++ phase3/rdt2_2.py
@@ -1,8 +1,14 @@
 from random import randrange
 import socket
 
+# turn debug_print on/off to reduce time if needed
+debug = True
+def debug_print(message):
+    if debug:
+        print(message)
+
 class RDT2_2:
-    ACK = 0x00
+    ACK = 0x00 # 8 bit / 1 byte size
     def __init__(self, send_address, send_port, recv_address, recv_port, packet_size=1024, corruption=0, option=[1, 2, 3]):
         # Set the basic connection and configuration parameters
         self.send_address = send_address
@@ -17,7 +23,7 @@ class RDT2_2:
         # Initialize FSM state and header size
         self._state = 0
         self._prev_state = 1
-        self._header_size = 3
+        self._header_size = 3 # size in byte
 
         # Create sender and receiver sockets
         self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,7 +32,7 @@ class RDT2_2:
 
     def send(self, packets):
         # Inform the user about the total number of packets to be sent
-        print(f"RDT2_2 - packet count = {len(packets)}")
+        debug_print(f"RDT2_2 - packet count = {len(packets)}")
 
         while True:
             # Send the header with the number of packets to be transmitted and wait for an acknowledgment
@@ -42,14 +48,14 @@ class RDT2_2:
                 break
             else:
                 # Handle the case where acknowledgment is not received or state does not match
-                print("RDT2_2: FSM STATE DOES NOT MATCH")
+                debug_print("RDT2_2: FSM STATE DOES NOT MATCH")
                 continue
 
         packet_ide = 0
 
         while packet_ide < len(packets):
             # Inform the user about the packet being sent
-            print(f"RDT2_2 - Sending packet : {packet_ide}/{len(packets) - 1}")
+            debug_print(f"RDT2_2 - Sending packet : {packet_ide}/{len(packets) - 1}")
 
             # Send the packet
             self.send_sock.sendto(self._add_header(packets[packet_ide], self._state), (self.send_address, self.send_port))
@@ -67,7 +73,7 @@ class RDT2_2:
                 packet_ide += 1
             else:
                 # Handle the case where acknowledgment is not received or state does not match
-                print("RDT2_2 - FSM STATE DOES NOT MATCH")
+                debug_print("RDT2_2 - FSM STATE DOES NOT MATCH")
 
         # Reset the FSM state after sending all packets
         self._reset()
@@ -78,7 +84,7 @@ class RDT2_2:
         packet_ide = 0
 
         while True:
-            print("RDT2_2 - Receiving packet count")
+            debug_print("RDT2_2 - Receiving packet count")
 
             packet, address = self.recv_sock.recvfrom(1024 + self._header_size)
             header, data, checksum = self._parse_packet(packet)
@@ -88,24 +94,23 @@ class RDT2_2:
             # validating the checksum
             # checking whether the state of the sending process matches the state of the sending process.
             # The selected option is acted
-            if ((not ((self._corrupted()) and (3 in self.option))) or (1 in self.option)) and self._verify_checksum(
-                    packet):
+            if ((not ((self._corrupted()) and (3 in self.option))) or (1 in self.option)) and self._verify_checksum(packet):
                 if (header == self._state):
                     # Getting number of bytes to receive from the sending process
                     packet_cnt = data
-                    print(f"RDT2_2 - Packets that are receiving from sending process = {packet_cnt} packets")
+                    debug_print(f"RDT2_2 - Packets that are receiving from sending process = {packet_cnt} packets")
                     self._send_ack(self._state)
                     self._change_state()
                     break
                 else:
-                    print("RDT2_2 - Packet with that are mismatched State are received.")
+                    debug_print("RDT2_2 - Packet with that are mismatched State are received.")
                     self._send_ack(self._prev_state)
             else:
-                print("RDT2_2 - Corrupted packet is received.")
+                debug_print("RDT2_2 - Corrupted packet is received.")
                 self._send_ack(self._prev_state)
         # Receive the number of packets indicated by the sending process.
         while packet_ide < packet_cnt:
-            print(f"RDT2_2 - Receiving packet {packet_ide}/{packet_cnt - 1} from sending process")
+            debug_print(f"RDT2_2 - Receiving packet {packet_ide}/{packet_cnt - 1} from sending process")
 
             packet, address = self.recv_sock.recvfrom(self.packet_size + self._header_size)
             header, data, checksum = self._parse_packet(packet)
@@ -120,91 +125,96 @@ class RDT2_2:
                 # changing and packet data collection process.
                 if (header == self._state):
                     # Add the received packet to the packet list.
-                    print(f"RDT2_2: Packet {packet_ide} Received.")
+                    debug_print(f"RDT2_2: Packet {packet_ide} Received.")
                     packet_data.append(data)
                     packet_ide += 1
                     self._send_ack(self._state)
                     self._change_state()
                 else:
-                    print("RDT2_2: Packet with Mismatched State Received.")
+                    debug_print("RDT2_2: Packet with Mismatched State Received.")
                     self._send_ack(self._prev_state)
             else:
-                print("RDT2_2: Corrupted Packet Received.")
+                debug_print("RDT2_2: Corrupted Packet Received.")
                 self._send_ack(self._prev_state)
 
         self._reset()
         return packet_data
 
-    def _is_packet_valid(self, packet, header):
-        checksum = packet[-2:]
-        return ((not self._corrupted() and 3 in self.option) or 1 in self.option) and self._verify_checksum(packet) and header == self._state
-
     # checking for the ACK state
     def _send_ack(self, state):
-        print(f"RDT2_2 - Sending ACK{state}")
-        packet = self._add_header(self.ACK.to_bytes(1, 'big'), state)
-        self.send_sock.sendto(packet, (self.send_address, self.send_port))
-        return
+        # Inform the user about sending an acknowledgment
+        debug_print(f"RDT2_2 - Sending ACK {state}")
+    
+        # Create an acknowledgment packet with the given state
+        ack_packet = self._add_header(self.ACK.to_bytes(1, 'big'), state)
 
-    # waiting for the ACK and NACK response
+        # Send the acknowledgment packet to the sender
+        self.send_sock.sendto(ack_packet, (self.send_address, self.send_port))
+
+    # waiting for the ACK response
     def _recv_ack(self):
-        msg = None
-        while msg is None:
-            msg, address = self.recv_sock.recvfrom(1024)
+        received_packet, sender_address = None, None
 
-        # getting the data from the ACK
-        header, data, checksum = self._parse_packet(msg)
+        # Keep receiving packets until a valid ACK or NAK is received
+        while received_packet is None:
+            received_packet, sender_address = self.recv_sock.recvfrom(1024)
+    
+        # Parse the received acknowledgment packet
+        header, data, checksum = self._parse_packet(received_packet)
 
-        # validating the checksum of the packet and also the state ,ACK/NAK status of received response packet.
-        if ((not ((self._corrupted()) and (2 in self.option))) or (1 in self.option)) and self._verify_checksum(msg):
-            if (header == self._state) and (int.from_bytes(data, 'big') == self.ACK):
-                print("RDT2_2 - ACK")
+        # Validate the checksum of the packet and check the state and ACK/NAK status
+        if ((not ((self._corrupted()) and (2 in self.option))) or (1 in self.option)) and self._verify_checksum(received_packet):
+            if header == self._state and int.from_bytes(data, 'big') == self.ACK:
+                # Acknowledgment received successfully
+                debug_print("RDT2_2 - ACK")
                 return True, header
-            elif (header != self._state) and (int.from_bytes(data, 'big') == self.ACK):
-                print("RDT2_2 - ACK with Unmatch")
+            elif header != self._state and int.from_bytes(data, 'big') == self.ACK:
+                # Acknowledgment received with a mismatched state
+                debug_print("RDT2_2 - ACK with Unmatch")
                 return True, header
             else:
-                print("RDT2_2 - Non-ACK Response")
+                # Non-ACK Response received
+                debug_print("RDT2_2 - Non-ACK Response")
                 return False, header
         else:
-            print("RDT2_2 - Corrupted packet")
+            # Corrupted packet received
+            debug_print("RDT2_2 - Corrupted packet")
             return False, header
 
-
    # getting the header messgae
-
     def _add_header(self, packet, state):
-        header = state.to_bytes(1, byteorder='big')  # FSM State.
-        checksum = self._checksum(header + packet)  # Checksum calculation.
-
-        return header + packet + checksum
+        # Create a header with the FSM state
+        header = state.to_bytes(1, byteorder='big')
+    
+        # Calculate the checksum for the header and packet
+        header_checksum = self._checksum(header + packet)
+    
+        # Combine the header, packet, and checksum to form the packet with header
+        header_packet = header + packet + header_checksum
+    
+        return header_packet
 
     def _parse_packet(self, packet):
-        header = packet[0]  # Extract the header bytes.
-        data = packet[1:(len(packet) - 2)]  # Extract the packet application data.
-        checksum = packet[-2:]  # Extract the packet checksum bytes.
+        # Extract the header, data, and checksum from the packet
+        header = packet[0]
+        data = packet[1:-2]
+        checksum = packet[-2:]
+    
         return header, data, checksum
 
-
-   # state is determined
     def _change_state(self):
-        self._prev_state = self._state
-        self._state = self._state ^ 1
-        return
+        # Toggle the FSM state between 0 and 1
+        self._prev_state, self._state = self._state, self._prev_state
 
-    # corruption range is estimated with the option selected
     def _corrupted(self):
-        if self.corruption >= randrange(1, 101):
-            return True
-        else:
-            return False
-
+        # Check if the packet should be considered corrupted based on the corruption rate
+        return self.corruption >= randrange(1, 101)
 
     def _reset(self):
-        print("RDT2_2 - resetting the process.")
+        debug_print("RDT2_2 - resetting the process.")
         self._state = 0
 
-    # estimating the checksum
+    # estimating the checksum, from geekfromgeek
     def _checksum(self, packet):
         sum_ = 0
         k = 16
